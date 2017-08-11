0cc08168a3aa823e542b8007eb6e676f9f8b8a32
lioneltrebuchon <ltrebuchon@gmail.com>
2017-08-11 08:39:01 -0700

    [video_original_2]: verified script is working, but need letter in front of numbers.

--- script_video_original2.sh
+++ script_video_original2.sh
@@ -14,7 +14,7 @@ printf "\n"
 
 # Initializations
 mkdir -p tmp
-i=1
+i=3
 space=" "
 toto="toti"
 path="$path"
@@ -22,18 +22,17 @@ path="$path"
 #  Transform files into something useable by avconv -i
 while [ $i -ne 0 ]
 do	 
-	oldtoto=$toto
+    oldtoto=$toto
 	toto=$(find -maxdepth 1 -name *[^0-9]$i'.png')
 	ln -fs ../"$toto" tmp/img"$i".png;
 
 	if [ $i -eq 1 ]
 	then
 		name=$toto
-	fi
+    fi
 	if [ "$toto" == "$oldtoto" ]
 	then
 		i=0
-		echo "End of execution with two errors."
 	else
 		i=$(($i + 1))
 	fi
@@ -42,8 +41,9 @@ unset i
 
 # Create movie
 # NO OUTPUT
-avconv -loglevel panic -start_number 1 -i tmp/img%d.png -r 12 -qscale 10 -b:v 1M "${name::-10}".avi
 # DEBUG
+# avconv -loglevel panic -start_number 1 -i tmp/img%d.png -r 12 -qscale 10 -b:v 1M "${name::-10}".avi
+avconv -loglevel panic -start_number 3 -i tmp/img%d.png -r 12 -qscale 10 -b:v 1M jabbarzadeh.avi
 # avconv -start_number 1 -i tmp/img%d.png -r 12 -qscale 10 -b:v 1M "$name".avi
 
 # Clean up
