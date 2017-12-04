6f680f77abdc59911d5aa3325d7cbfcff536dd91
11Ltrebuchon <Lionel.Trebuchon@sonova.com>
2017-12-04 08:45:23 +0100

    Temp lineCounter.py: debug works, main doesn't work (stops at the very end of writing with error like AttributeError: 'tuple' object has no attribute 'tell')

--- lineCounter.py
+++ lineCounter.py
@@ -28,21 +28,22 @@ def list_files_with_lines(startpath,*writeFile):
                         file = file[:-2]
                         print('{}{}'.format(subindent,file))
     else:
+        fileDict = {}
+        # Building dictionary by parsing all files in current directory, recursively!
+        for root, dirs, files in os.walk(startpath):
+            if any (file.endswith(".m") for file in files ):
+                for file in files:
+                    if file.endswith(".m"):
+                        fileFullName = os.path.join(root,os.path.relpath(file))
+                        with open(fileFullName) as f:
+                            fileFullName = fileFullName[:-2]
+                            fileDict[fileFullName] = sum(1 for line in f if line.strip() and not line.startswith('%')) 
         with xlsxwriter.Workbook(writeFile) as workbook:
             worksheet = workbook.add_worksheet()
             row = 0
             col = 0
-            fileDict = {}
-            # Building dictionary by parsing all files in current directory, recursively!
-            for root, dirs, files in os.walk(startpath):
-                if any (file.endswith(".m") for file in files ):
-                    for file in files:
-                        if file.endswith(".m"):
-                            fileFullName = os.path.join(root,os.path.abspath(file))
-                            fileFullName = fileFullName[:-2]
-                            fileDict[fileFullName] = sum(1 for line in file if line.strip() and not line.startswith('%')) 
             # Exporting the dictionary to an .xlsx file
-            for key in fileDict.keys():
+            for key,value in fileDict.items():
                 row += 1
                 worksheet.write(row,col,key)
                 # Unfruitful attempt at a multilevel dictionary.
@@ -52,24 +53,56 @@ def list_files_with_lines(startpath,*writeFile):
                 #         worksheet.write(row,col+2,fileDict[key][item][0])
                 #         row += 1
                 # else:
-                worksheet.write(row,col+1,fileDict[key])
-                row += 1
+                worksheet.write(row,col+1,value)
 
 
-writeFile = 'listDirectories.xlsx'
-list_files_with_lines(os.getcwd(),writeFile)
+writeFile = os.path.join(os.getcwd(),'./listDirectories.xlsx')
+# list_files_with_lines(os.getcwd(),writeFile)
+startpath = os.path.join(os.getcwd(),'../ClassifierTools')
+# list_files_with_lines(startpath,writeFile)
 
 
-#parses through files and saves to a dict
-names = {}
-for fn in glob.glob('*.txt'):
-    with open(fn) as f:
-        names[fn] = sum(1 for line in f if line.strip() and not line.startswith('%')) 
+debug = 1
+if debug:
+    fileDict = {}
+    startpath = os.path.join(os.getcwd(),'../ClassifierTools')
+    for root, dirs, files in os.walk(startpath):
+        for file in files:
+            if file.endswith(".m"):
+                fileFullName = os.path.join(root,os.path.relpath(file))
+                with open(fileFullName) as f:
+                    fileFullName = fileFullName[:-2]
+                    fileDict[fileFullName] = sum(1 for line in f if line.strip() and not line.startswith('%')) 
 
-print names
+    print fileDict
 
-#save the dictionary with key/val pairs to a csv
-with open('matlabLineCounter.csv', 'w') as writeFile: 
-    writer = csv.writer(writeFile)
-    for key,value in names.items():
-        writer.writerow([key[:-2],value])
+    with xlsxwriter.Workbook(writeFile) as workbook:
+        worksheet = workbook.add_worksheet()
+        row = 0
+        col = 0
+        # Exporting the dictionary to an .xlsx file
+        for key in fileDict.keys():
+            row += 1
+            worksheet.write(row,col,key)
+            # Unfruitful attempt at a multilevel dictionary.
+            # if len(fileDict[key])>1:
+            #     for item in fileDict[key]:
+            #         worksheet.write(row,col+1,item)
+            #         worksheet.write(row,col+2,fileDict[key][item][0])
+            #         row += 1
+            # else:
+            worksheet.write(row,col+1,fileDict[key])
+
+    #parses through files and saves to a dict
+    names = {}
+    for fn in glob.glob('*.sh'):
+        with open(fn) as f:
+            names[fn] = sum(1 for line in f if line.strip() and not line.startswith('%')) 
+
+    print names
+
+    #save the dictionary with key/val pairs to a csv
+    with open('matlabLineCounter.csv', 'w') as writeFile: 
+        writer = csv.writer(writeFile)
+        for key,value in names.items():
+            writer.writerow([key[:-2],value])
