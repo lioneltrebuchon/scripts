6d7d1f46c8497357f61cb0fc07e9fcfa326619db
11Ltrebuchon <Lionel.Trebuchon@sonova.com>
2017-12-04 14:14:52 +0100

    Temp not working. Trying to have a file editing capability in the script as well.

--- lineCounter.py
+++ lineCounter.py
@@ -6,9 +6,11 @@ import os
 import sys
 import glob
 import xlsxwriter
+import fileinput
 
 def list_files_with_lines(startpath,*writeFile):
     if not writeFile:
+        # Printing file structure to command line
         for root, dirs, files in os.walk(startpath):
             flag = False
             if any (file.endswith(".m") for file in files ):
@@ -28,22 +30,49 @@ def list_files_with_lines(startpath,*writeFile):
                         file = file[:-2]
                         print('{}{}'.format(subindent,file))
     else:
+        extCounter = 0
+        print writeFile[1]
+        lengthCut = len(startpath)
         fileDict = {}
         # Building dictionary by parsing all files in current directory, recursively!
         for root, dirs, files in os.walk(startpath):
-            if any (file.endswith(".m") for file in files ):
-                for file in files:
-                    if file.endswith(".m"):
-                        fileFullName = os.path.join(root,os.path.relpath(file))
-                        with open(fileFullName) as f:
-                            fileFullName = fileFullName[:-2]
-                            fileDict[fileFullName] = sum(1 for line in f if line.strip() and not line.startswith('%')) 
-        with xlsxwriter.Workbook(writeFile) as workbook:
+        # if any (file.endswith(".m") for file in files ): #not needed
+            for file in files:
+                if file.endswith(".m"):
+                    fileFullName = os.path.join(root,os.path.relpath(file))
+                    f = open(fileFullName)
+                    keyName = fileFullName[lengthCut:-2]
+                    fileDict[keyName] = sum(1 for line in f if line.strip() and not line.startswith('%'))
+                    f.close()
+
+                    if writeFile[1]: # we'll be writing to line Nr. writeFile[1] if some condition is met
+                        comment = "%Code lines - "+str(fileDict[keyName])
+                        print comment
+                        print keyName
+                        if extCounter == 1:
+                            break
+                        extCounter += 1
+                        commentPosition = 1
+                        counter = 0
+                        for line in fileinput.FileInput(fileFullName,inplace=1):
+                            print "%d: %s" % (fileinput.filelineno(), line)
+                            if line[-3:] == "...":
+                                commentPosition += 1
+                                continue
+                            if counter == commentPosition and line[:12] != "%Code lines -":
+                                line=line.replace(line,line+comment)
+                                break
+                            elif line[:12] == "%Code lines -":
+                                line=line.replace(line,comment)
+                                break
+                            counter += 1
+
+        with xlsxwriter.Workbook(writeFile[0]) as workbook:
             worksheet = workbook.add_worksheet()
             row = 0
             col = 0
             # Exporting the dictionary to an .xlsx file
-            for key,value in fileDict.items():
+            for key in fileDict.keys():
                 row += 1
                 worksheet.write(row,col,key)
                 # Unfruitful attempt at a multilevel dictionary.
@@ -53,46 +82,16 @@ def list_files_with_lines(startpath,*writeFile):
                 #         worksheet.write(row,col+2,fileDict[key][item][0])
                 #         row += 1
                 # else:
-                worksheet.write(row,col+1,value)
+                worksheet.write(row,col+1,fileDict[key])
 
 
 writeFile = os.path.join(os.getcwd(),'./listDirectories.xlsx')
-# list_files_with_lines(os.getcwd(),writeFile)
-startpath = os.path.join(os.getcwd(),'../ClassifierTools')
-# list_files_with_lines(startpath,writeFile)
+startpath = os.path.join(os.getcwd(),'../temp')
+list_files_with_lines(startpath,writeFile,1)
 
 
-debug = 1
+debug = 0
 if debug:
-    fileDict = {}
-    startpath = os.path.join(os.getcwd(),'../ClassifierTools')
-    for root, dirs, files in os.walk(startpath):
-        for file in files:
-            if file.endswith(".m"):
-                fileFullName = os.path.join(root,os.path.relpath(file))
-                with open(fileFullName) as f:
-                    fileFullName = fileFullName[:-2]
-                    fileDict[fileFullName] = sum(1 for line in f if line.strip() and not line.startswith('%')) 
-
-    print fileDict
-
-    with xlsxwriter.Workbook(writeFile) as workbook:
-        worksheet = workbook.add_worksheet()
-        row = 0
-        col = 0
-        # Exporting the dictionary to an .xlsx file
-        for key in fileDict.keys():
-            row += 1
-            worksheet.write(row,col,key)
-            # Unfruitful attempt at a multilevel dictionary.
-            # if len(fileDict[key])>1:
-            #     for item in fileDict[key]:
-            #         worksheet.write(row,col+1,item)
-            #         worksheet.write(row,col+2,fileDict[key][item][0])
-            #         row += 1
-            # else:
-            worksheet.write(row,col+1,fileDict[key])
-
     #parses through files and saves to a dict
     names = {}
     for fn in glob.glob('*.sh'):
